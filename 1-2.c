d9e0645bea7cd50663759f7aa9a8fe728a6ad15d
shelkeswayam681-max <shelkeswayam681@gmail.com>
2025-12-05 14:52:34 +0530

    exp 1.2 added

---
+++ 1-2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    float phy, chem, math, avg;
+    printf("Enter marks for Physics: ");
+    scanf("%f", &phy);
+    printf("Enter sarks for Chemistry: ");
+    scanf("%f", &chem);
+    printf("Enter marks for Mathematics: ");
+    scanf("%f", &math);
+    avg = (phy+chem+math)/3;
+    printf("\nAverage Marks: %2f\n", avg);
+    (avg>=50)? printf("You are Eligible") : printf("You are Not Eligible");
+
+    return 0;
+}
