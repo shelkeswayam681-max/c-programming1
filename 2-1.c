44c84b140c506d2a39f946e05ec95c38ad565221
shelkeswayam681-max <shelkeswayam681@gmail.com>
2025-12-05 15:03:55 +0530

    Add files via upload
    exp 2 added

---
+++ 2-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+int num;
+printf("Enter a Number:");
+scanf("%d",&num);
+
+if(num%2 == 0){
+    printf("Even Number");
+}
+else{
+    printf("Odd Number");
+}
+    return 0;
+}
