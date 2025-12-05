a078352fdba28f4bc4713035abd7e3a9107f74f7
shelkeswayam681-max <shelkeswayam681@gmail.com>
2025-12-05 14:49:05 +0530

    exp 1.1 added

---
+++ 1-1.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(){
+   
+
+printf("Name: swayam shelke\n");
+printf("Age: 18\n");
+printf("Class: First Year\n");
+printf("Division: D\n");
+printf("UIN: 251P095\n");
+
+return 0;
+}
